ab7e71b3231a280233f72ab977c3abf9165d862e
shayneofficer <shayneofficer@gmail.com>
2018-11-11 20:37:15 -0600

    added some finishing touches, audio element

--- README.md
+++ README.md
@@ -5,7 +5,11 @@ Hangman with a theme of Netflix original series
 
 ## TODO
 1. Display a message, increase win count, and reset game when a word is guessed correctly.
+    - Fixed! Added `alert("You win!")` and increased the win count, reset the game, but the last correct guess isn't printed.
 2. Fix bug: when a player fails to guess a word correctly, the next word's letters are shown based on guesses from the previous round (these vanish when a player starts guessing, but it still gives away most of the word)
+    - Fixed! The `resetGame()` function was writing the underscore string *before* emptying the guesses array, just had to switch the order.
 3. Get spaces to show up in the underscore string. The if statement appears to recognize that spaces are present, but doesn't execute the associated block of code. Alternatively, concatenating the string "  " is ignored?
+    - It seems like the spaces are showing up, just aren't very noticeable...
 4. Refactor: more of the code could be split out into reused functions. Similarly, functions like `writeCurrentWord()` seem inefficient.
 5. Style to fit Netflix theme
+    - Mostly done. It isn't very responsive, but it can't be played without a keyboard anyway, so...
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,9 @@
 // Counters for wins and guesses
 var wins = 0;
-var guessesRemaining = 12;
+var guessesRemaining = 10;
+
+var audioElement = document.createElement("audio");
+audioElement.setAttribute("src", "assets/netflix-opening.wav");
 
 // Array of strings to guess
 var titles = [
@@ -83,6 +86,7 @@ var guesses = [];
 var wordBox = document.getElementById("word-box");
 var lettersGuessedBox = document.getElementById("letters-guessed-box");
 var numberRemainingGuessesBox = document.getElementById("missed-box");
+var winsBox = document.getElementById("wins-box");
 
 // Generate a random index of the titles array and return the string at that index
 function pickTitle() {
@@ -101,22 +105,22 @@ function writeCurrentWord(word) {
     // Iterate through all the characters of the chosen string (except the last)
     for (var i = 0; i < word.length - 1; i++) {
         if (word.charAt(i).toUpperCase() === " ") { // If the character at this index of the string is a space...
-            currentWord += "\ \ "; // ... add a space to the underscores string
+            currentWord += "\ \ "; //... add a space to the underscores string
         }
         else if (!guesses.includes(word.charAt(i).toUpperCase())) { // If the character at the index hasn't aleady been guessed...
-            currentWord += "_ "; // ... add an underscore to the underscores string
+            currentWord += "_ "; //... add an underscore to the underscores string
         }
         else { // If the character isn't a space and *has* been guessed...
-            currentWord += word.charAt(i); // ... add the guessed character to our underscores string
+            currentWord += word.charAt(i); //... add the guessed character to our underscores string
         }
     }
 
     // Then we handle the final character of our string
     if (!guesses.includes(word.charAt(word.length - 1).toUpperCase())) { // If the final character hasn't already been guessed...
-        currentWord += "_"; // ... add an underscore to our underscores string (note no trailing space)
+        currentWord += "_"; //... add an underscore to our underscores string (note no trailing space)
     }
     else { // If the final character *has* already been guessed
-        currentWord += word.charAt(word.length - 1); // ... add the guessed character to our underscores string
+        currentWord += word.charAt(word.length - 1); // .. add the guessed character to our underscores string
     }
 
     // Print the underscores string to our word box on screen
@@ -139,35 +143,43 @@ function writeGuessedCharacters() {
 }
 
 function resetGame() {
-    computerPick = pickTitle(); // Pick a new string to guess...
-    writeCurrentWord(computerPick); // ... print the underscore string...
-    guessesRemaining = 12; // ... reset guesses remaining...
-    numberRemainingGuessesBox.textContent = guessesRemaining; // ... print our reset guess count...
-    guesses = []; // ... reset the guesses array...
-    lettersGuessedBox.textContent = ""; // ... print our empty guesses array...
-
+    guesses = []; // Reset the guesses array...
+    computerPick = pickTitle(); //... pick a new string to guess...
+    writeCurrentWord(computerPick); //... print the underscore string...
+    guessesRemaining = 10; //... reset guesses remaining...
+    numberRemainingGuessesBox.textContent = guessesRemaining; //... print our reset guess count...
+    lettersGuessedBox.textContent = ""; //... print our empty guesses array...
+    winsBox.textContent = wins; //... print new win total
 }
+
 
 document.onkeyup = function (event) {
     userInput = event.key;
     userInput = userInput.toUpperCase();
 
     if (alphabet.includes(userInput) && !guesses.includes(userInput)) { // If the input is a letter, and hasn't already been guessed
-        guesses.push(userInput); // ... add the letter to our guesses array
+        guesses.push(userInput); //... add the letter to our guesses array
 
         if (computerPick.toUpperCase().includes(userInput)) { // If the input is in our string (correct guess)...
-            writeCurrentWord(computerPick); // ... rewrite the underscores string with correct guess filled in
+            writeCurrentWord(computerPick); //... rewrite the underscores string with correct guess filled in
         }
         else { // If the input is *not* in our string (incorrect guess)...
-            writeGuessedCharacters(); // ... add the guessed letter to our printed guessed characters...
-            guessesRemaining--; // ... and remove one guess remaining...
+            writeGuessedCharacters(); //... add the guessed letter to our printed guessed characters...
+            guessesRemaining--; //... and remove one guess remaining...
             numberRemainingGuessesBox.textContent = guessesRemaining;
         }
 
     } // If the input isn't a letter, do nothing
 
     if (guessesRemaining === 0) { // If we've run out of guesses...
-        resetGame(); // ... reset the game
+        resetGame(); //... reset the game
+    }
+
+    if (!wordBox.textContent.includes("_")) { // If there are no more underscores...
+        audioElement.play(); //... play Netflix chime...
+        wins++; //... add a win to the counter...
+        // alert("You win!"); //... alert the user...
+        resetGame(); //... reset the game
     }
 
 }
